59189d31aa85e4fddfc3c86a216463bd1fbe3ad8
Adil <madilaslam9@gmail.com>
2026-06-19 07:22:43 +0500

    Refactor Read Once

--- src/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from fastapi.testclient import TestClient
-
-client = TestClient(app)
-response = client.get("/check-user?address=0x83C549cC62fEd4798545d945daFB597186981E9c")
-print(response.status_code)
-print(response.text)
